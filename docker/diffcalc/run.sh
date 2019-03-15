2ebce66c7f20985de961446e765fbd72c7e87781
smoogipoo <smoogipoo@smgi.me>
2019-03-15 12:55:37 +0900

    Fix diffcalc run script

--- docker/diffcalc/run.sh
+++ docker/diffcalc/run.sh
@@ -11,8 +11,10 @@ echo "{
   \"ConnectionStrings\": {
     \"master\": \"Server=db;Database=osu;Uid=osuweb;SslMode=None;\"
   },
+  \"insert_beatmaps\": false,
   \"beatmaps_path\": \"/beatmaps\",
-  \"allow_download\": false
+  \"allow_download\": true,
+  \"download_path\": \"https://osu.ppy.sh/osu/{0}\"
 }" > appsettings.json
 
 dotnet osu.Server.DifficultyCalculator.dll all -m ${MODE} -c ${THREADS}
