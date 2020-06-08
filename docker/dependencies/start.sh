9f2291d653846954d70429eb32f85c0e79426bd4
smoogipoo <smoogipoo@smgi.me>
2020-06-08 15:33:36 +0900

    Cleanup db hax in start script

--- docker/dependencies/start.sh
+++ docker/dependencies/start.sh
@@ -23,17 +23,34 @@ echo "ES_SCORES_HOST=elasticsearch:9200" >> .env
 echo "QUERY_DETECTOR_ENABLED=0" >> .env
 echo "Importing data..."
 
-cat ./sql/*.sql | mysql -u osuweb --host=db --database=osu
+$SQL_CONN='mysql -u osuweb --host=db --database=osu'
 
-echo "INSERT INTO phpbb_users (user_id,username,user_warnings,user_type,user_permissions,user_sig,user_occ,user_interests,username_clean,country_acronym) SELECT user_id,username,user_warnings,user_type,0,'','','',username,'AU' FROM sample_users;
- UPDATE phpbb_users SET user_lastvisit=4294967295, osu_playmode=${MODE} WHERE 1;
- INSERT INTO osu_countries (acronym,name,rankedscore,playcount) VALUES ('AU','Australia',0,0);
- INSERT INTO osu_genres (genre_id,name) VALUES (0, 'genre-1');
- INSERT INTO osu_languages (name) VALUES ('language-1');
- UPDATE osu_beatmapsets SET genre_id=1,language_id=1 WHERE 1;
- INSERT INTO osu_counts (name, count) VALUES ('docker_db_step', '1');
- INSERT INTO osu_user_performance_rank (user_id, mode, r0) SELECT user_id, ${MODE}, 1 FROM phpbb_users;" | mysql -u osuweb --host=db --database=osu
+# Initial import.
+cat ./sql/*.sql \
+  | $SQL_CONN
 
+# Move sample users to phpbb_users (initial import).
+echo "INSERT INTO phpbb_users (user_id,username,user_warnings,user_type,user_permissions,user_sig,user_occ,user_interests,username_clean,country_acronym) SELECT user_id,username,user_warnings,user_type,0,'','','',username,'AU' FROM sample_users;" \
+  | $SQL_CONN
+
+# Sample data doesn't contain last visit times. Un-gray all users.
+echo "UPDATE phpbb_users SET user_lastvisit=4294967295, osu_playmode=${MODE} WHERE 1;" \
+  | $SQL_CONN
+
+# Add default country + performance rank (fixes errors on user page).
+echo "INSERT INTO osu_countries (acronym,name,rankedscore,playcount) VALUES ('AU','Australia',0,0);
+      INSERT INTO osu_user_performance_rank (user_id, mode, r0) SELECT user_id, ${MODE}, 1 FROM phpbb_users;" \
+  | $SQL_CONN
+
+# Add default genre + language (fixes errors on beatmap set page).
+echo "INSERT INTO osu_genres (genre_id,name) VALUES (0, 'genre-1');
+      INSERT INTO osu_languages (name) VALUES ('language-1');
+      UPDATE osu_beatmapsets SET genre_id=1,language_id=1 WHERE 1;" \
+  | $SQL_CONN
+
+# Notify of completion.
+echo "INSERT INTO osu_counts (name, count) VALUES ('docker_db_step', '1');" \
+  | $SQL_CONN
 echo "Finshed importing data."
 
 # undo config and route caching by the script above
