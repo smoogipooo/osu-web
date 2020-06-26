9d8138d41d542fb41f6d6e64e2393bf6dfba9532
smoogipoo <smoogipoo@smgi.me>
2020-06-26 18:19:09 +0900

    Update docker-compose

--- docker/diffcalc/run.sh
+++ docker/diffcalc/run.sh
@@ -2,20 +2,8 @@
 
 OUT_DIR='/out'
 
-dotnet build -c:Release -o ${OUT_DIR}
+dotnet run -c:Release -o ${OUT_DIR}
 
 cd ${OUT_DIR}
-
-echo "{
-  // comments are supported.
-  \"ConnectionStrings\": {
-    \"master\": \"Server=db;Database=osu;Uid=osuweb;SslMode=None;\"
-  },
-  \"insert_beatmaps\": false,
-  \"beatmaps_path\": \"/beatmaps\",
-  \"allow_download\": true,
-  \"download_path\": \"https://osu.ppy.sh/osu/{0}\",
-  \"save_downloaded\": true
-}" > appsettings.json
 
 dotnet osu.Server.DifficultyCalculator.dll all -m ${MODE} -c ${THREADS} -ac
